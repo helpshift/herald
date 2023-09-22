b77e1a1435eaeff4815ae2c0771d064463c712da
Jean-Marc Le Peuvédic <lepeuvedic@gmail.com>
2023-09-23 01:31:33 +0200

    📝 Add __main__.py module
    Transforms herald in an executable module. Must run using -m option of the Python 3 interpreter. The plugins directory is no longer a module since plugins just Python scripts which import herald module herald.baseplugin.
    © CalCool Studios SAS 2021-2023

---
+++ herald/__main__.py
@@ -0,0 +1,18 @@
+""" Module __main__.py 
+
+    
+
+    Created on 23/09/2023
+    By Jean-Marc Le Peuvédic
+    © CalCool Studios SAS 2021-2023
+"""
+# TODO List
+
+# Import system libraries
+
+# Import public libraries
+
+# Import application modules
+from herald.herald import main
+
+main()
