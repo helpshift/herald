d5db10f7ebd80d8838c2e44b485de71521d17ed4
Jean-Marc Le Peuvédic <lepeuvedic@gmail.com>
2023-09-27 21:25:33 +0200

    Exclude whole .idea from Git

--- herald/__main__.py
+++ herald/__main__.py
@@ -1,19 +1,9 @@
-""" Module __main__.py 
-
-    
-
-    Created on 23/09/2023
-    By Jean-Marc Le Peuvédic
-    © CalCool Studios SAS 2021-2023
-"""
 # Import system libraries
 import sys
 
 if sys.gettrace() is None:
     from gevent import monkey
     monkey.patch_all()
-
-# Import public libraries
 
 # Import application modules
 from herald.herald import main
